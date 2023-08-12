70324814bbfbcf63c7d03384d085675928af47b8
sk-py <77550660+sk-py@users.noreply.github.com>
2023-08-12 23:38:43 +0530

    Assets folder added.
    Just make sure to add these files in assets folder

---
+++ images.js
@@ -0,0 +1,9 @@
+import image1 from './pexels-shotpot-4046705.jpg';
+import image2 from './pexels-ivan-samkov-4164849.jpg';
+import image3 from './pexels-ibrahim-hafedh-16216617.jpg';
+import image4 from './pic4.jpg';
+import image5 from './spacious.jpg';
+
+
+
+export default [image1,image2,image3,image4,image5]
